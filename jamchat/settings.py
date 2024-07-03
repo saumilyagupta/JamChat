ce8f921570ecaa3b55b5c042d4c25ee45d8fce78
Saumilya JJ <82203974+saumilyagupta@users.noreply.github.com>
2024-07-03 19:55:27 +0530

    Update settings.py

--- jamchat/settings.py
+++ jamchat/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8r$##r_tp=--$ed$ty$%%(t#wgnw*=3al183sp545*uiq5j#-6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
